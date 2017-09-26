7cc90735195e9503a59f59f6e96681fb8cef5497
Deams51 <mfourgeaud@gmail.com>
2017-09-26 22:46:34 +0200

    Debug output

--- index.js
+++ index.js
@@ -128,12 +128,15 @@ function cleanData(stringArray) {
     const res = [];
     // Split lines, using a sync loop to keep order
     const dataArray = stringArray.toString().split(/\r?\n/);
-    for(const idx in dataArray) {
+    console.log(stringArray);
+    console.log(dataArray);
+    for (var i = 0, len = dataArray.length; i < len; i++) {
         const msg = dataArray[idx];
         if(msg.replace(/\s/g, '').length > 0) {
             // Remove unicode characters and push to results
             res.push(msg.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '').replace(/\r?\n|\r/g, ''));
         }
     }
+    console.log(res);
     return res;
 }
